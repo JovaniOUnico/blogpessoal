15dc2f8cbea6279464739bea4db39e45f82c6925
JovaniOUnico <jovani.souza.almeida@gmail.com>
2025-05-27 13:21:00 -0300

    Removendo validação ao logar (need fix)

--- src/test/java/com/generation/blogpessoal/util/TestBuilder.java
+++ src/test/java/com/generation/blogpessoal/util/TestBuilder.java
@@ -1,11 +1,22 @@
 package com.generation.blogpessoal.util;
 
 import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.model.UsuarioLogin;
 
 public class TestBuilder {
 
     public static Usuario criarUsuario(Long id, String nome, String email, String senha) {
         Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNome(nome);
+        usuario.setUsuario(email);
+        usuario.setSenha(senha);
+        usuario.setFoto("-");
+        return usuario;
+    }
+    
+    public static UsuarioLogin criarUsuarioLogin(Long id, String nome, String email, String senha) {
+        UsuarioLogin usuario = new UsuarioLogin();
         usuario.setId(id);
         usuario.setNome(nome);
         usuario.setUsuario(email);
